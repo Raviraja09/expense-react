7b812f679b6712f5a0803233f712030047af07d4
Raviraja09 <rajavysyaraju09@gmail.com>
2022-11-27 12:19:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Expense items</h2>
+      <ExpenseItem></ExpenseItem>
+
+     
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem()
+{
+    return(
+        <div>
+            <div>Expense items</div>
+            <div>
+            <h2>food Rs 100</h2>
+            <h2>petrol Rs 100</h2>
+            <h2>movies Rs 100</h2>
+            
+        </div>
+        </div>
+    );
+}
+export default ExpenseItem;
