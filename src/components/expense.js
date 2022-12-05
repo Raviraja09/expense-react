62f08c2914c81088bcad4dea79d5192ba64d3063
Raviraja09 <rajavysyaraju09@gmail.com>
2022-12-05 23:06:17 +0530

    filter

--- src/components/expense.js
+++ src/components/expense.js
@@ -6,13 +6,19 @@ const expenses=(props)=>
   const filterchangeHandler=selectyear=>{
     setFilteredyear(selectyear);
   }
+  const Filteredexpenses=props.items.filter(expense=>{
+    return expense.date.getfullyear().toString()===filteredyear;
+  })
 return(
   <div>
     <card classname='expense'>
-      <expensesFilter selected={Filteredyear} onchangeFilter={filterchangeHandler}/>
-      {props.items.map(expense=><ExpenseItem>
+      <expensesFilter selected={Filteredyear} 
+      onchangeFilter={filterchangeHandler}/>
+      {Filteredexpenses.map(expense=><ExpenseItem>
+        key={expense.id}
          title={expense.title} 
          amount={expense.amount}
+         date={expense.date}
          </ExpenseItem>
       
       )}
